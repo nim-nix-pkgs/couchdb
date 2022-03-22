ad79800ac075dc293f10ad9f27113190e385f9fe
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:49 -0300

    chore: re-index ../nimpkgs/c/couchdb/master

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''A library for managing your CouchDB. Easy & comfortably to use.'';
+  description = ''Easily and comfortably access your CouchDB.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-couchdb-master.flake = false;
   inputs.src-couchdb-master.owner = "theAkito";
-  inputs.src-couchdb-master.ref   = "refs/heads/master";
+  inputs.src-couchdb-master.ref   = "master";
   inputs.src-couchdb-master.repo  = "nim-couchdb";
   inputs.src-couchdb-master.type  = "github";
   
