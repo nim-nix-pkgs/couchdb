8fbab73f035cbae27bb1c33dbb832425ceef9f60
hugosenari <hugosenari@gmail.com>
2022-03-27 21:48:48 -0300

    chore: re index couchdb

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-couchdb-master.ref   = "refs/heads/master";
   inputs.src-couchdb-master.owner = "theAkito";
   inputs.src-couchdb-master.repo  = "nim-couchdb";
-  inputs.src-couchdb-master.dir   = "";
   inputs.src-couchdb-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
